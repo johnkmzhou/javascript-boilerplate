90d72dc039bc6ca82e395f80b579513772f79824
jzhou <john_zhou@hid.uscourts.gov>
2020-06-03 17:13:38 -1000

    Update dependencies.

--- antd-web/config-overrides.js
+++ antd-web/config-overrides.js
@@ -1,14 +1,15 @@
-const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: true
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
   }),
   addLessLoader({
-    javascriptEnabled: true,
-    modifyVars: {
-    }
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: {},
+    },
   })
 );
